b720fafa13329a8d628ccaeda30d3fc5c562bb8f
Ella Hoffman <e-hoffman.2@onu.edu>
2024-04-25 09:39:48 -0400

    Intermediate Commit

--- Source.cpp
+++ Source.cpp
@@ -1,1 +1,40 @@
- 
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+void pressEnterToContinue() {
+    cout << "Press enter to continue...";
+    cin.get();
+}
+
+int main() {
+    string filename;
+    cout << "input file: ";
+    getline(cin, filename);
+
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error opening file. \n";
+        pressEnterToContinue();
+        return 1;
+    }
+
+    int wordCount = 0;
+    int charCount = 0;
+    string word;
+    while (file >> word) {
+        wordCount++;
+        for (char c : word) {
+            if (!isspace(c)) {
+                charCount++;
+            }
+        }
+    }
+
+    cout << "Words: " << wordCount << endl;
+    cout << "Characters: " << charCount << endl;
+    pressEnterToContinue();
+    return 0;
+}
